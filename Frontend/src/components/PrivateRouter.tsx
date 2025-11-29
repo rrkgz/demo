20fa843ac39f9aa31ff5349ba55b2d77d9af0077
rrkgz <nicolas.soto.rodriguez@gmail.com>
2025-11-29 19:18:16 -0300

    pruebagitkraken
    lol

--- Frontend/src/components/PrivateRouter.tsx
+++ Frontend/src/components/PrivateRouter.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-
+//xd
 export default function PrivateRoute(){
     const navigate = useNavigate();
     const token = localStorage.getItem('token');
